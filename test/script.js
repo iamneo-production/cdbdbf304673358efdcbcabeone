687a4bdfdfa6a54f1e1ab824040d79f3f312e12b
Iamneo <techadmin@iamneo.ai>
2023-09-03 09:21:38 +0000

    Backup Commit

--- test/script.js
+++ test/script.js
@@ -6,8 +6,20 @@ let gameBoard=['','','','','','','','',''];
 let gameActive=true;
 const winningCombinations=[[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
 const checkWinner=() =>{
-    for(let combo of winningCombinations){
+    for(let combo of winningCombinations)
+    {
         const[a,b,c]=combo;
-        if(gameBoard)[a]&&gameBoard[a]===gameBoard[b]&&gameBoard[a]===gameBoard[c]
+        if(gameBoard[a]&&gameBoard[a]===gameBoard[b]&&gameBoard[a]===gameBoard[c]){
+            resultText.textContent='Player$(gameBoard[a]} wins!';
+            gameActive=false;
+            resetButton.disabled=false;
+        }
     }
+    if(!gameBoard.includes('')&&gameActive){
+        resultText.textContent="It's a draw!";
+        resetButton.disabled=false;
+    }
+};
+const handleClick=(cell,index)=>{
+    if(gameBoard)
 }
