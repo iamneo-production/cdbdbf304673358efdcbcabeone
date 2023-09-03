3e1372b4ea1b83770c454eb4dc05c0ff6f19915f
Iamneo <techadmin@iamneo.ai>
2023-09-03 09:11:38 +0000

    Backup Commit

---
+++ test/script.js
@@ -0,0 +1,5 @@
+const cells=document.querySelectorAll('.bt');
+const resultText=document.querySelector('.result-text');
+const resetButton=document.getElementById('reset-Button');
+let currentPlayer='X';
+let gameBoard=['','','',]
